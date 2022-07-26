260658eb1060e324addc1c69f48bc25890c6706c
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2022-07-26 19:22:04 +0200

    initializing project with npm and getting sass up and running

---
+++ snowpack.config.js
@@ -0,0 +1,31 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  root: 'src/app',
+  // mount: {
+  //   public: {
+  //     url: "/",
+  //     // static: true
+  //   },
+  //   src: "/"
+  // },
+  plugins: [
+    "@snowpack/plugin-sass",
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    // openUrl: 'src/app/'
+  },
+  buildOptions: {
+    out: "dist"
+  },
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2015',
+  },
+};
