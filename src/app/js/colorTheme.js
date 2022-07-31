77deb0f38a2fc4a9757950ef8ba0f6425696a1da
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2022-07-31 14:52:04 +0200

    styling top header finished

--- src/app/js/colorTheme.js
+++ src/app/js/colorTheme.js
@@ -1,3 +1,6 @@
+
+const darkThemeBtn = document.querySelector('.dark-mode-btn');
+
 // determining color theme once the page is loaded
 window.onload = () => {
     const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -6,6 +9,12 @@ window.onload = () => {
 
 // watching for changing preferred color theme
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => setColorTheme(e.matches));
+
+// toggling dark/light theme
+darkThemeBtn.addEventListener('click', () => {
+    const isDark = document.body.dataset.colorTheme === 'dark';
+    setColorTheme(!isDark);
+});
 
 function setColorTheme(isDark) {
     const currentColorTheme = isDark ? 'dark' : 'light';
